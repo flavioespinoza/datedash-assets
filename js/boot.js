fb68690abf15a41994cbed3c3c8196254f9a4d6b
flavioespinoza <flavio.espinoza@gmail.com>
2019-03-15 07:41:46 -0600

    stuff

--- js/boot.js
+++ js/boot.js
@@ -1,37 +1,73 @@
-;(function(root) {
-    'use strict'
+---
+---
+(function(root) {
+  'use strict'
 
-    var head = document.head
-    var rootEl = document.documentElement
+  var head = document.head,
+      rootEl = document.documentElement
 
-    function addStyleSheet(res) {
-        var link = document.createElement('link')
+  function addStyleSheet(res) {
+    var link = document.createElement('link')
 
-        if (res.integrity && res.href.slice(0, 6) === 'https:') {
-            link.crossOrigin = 'anonymous'
-            link.integrity = res.integrity
-        }
-
-        link.rel = 'stylesheet'
-        link.href = res.href
-        head.appendChild(link)
+    if (res.integrity &&
+        res.href.slice(0, 6) === 'https:') {
+      link.crossOrigin = 'anonymous'
+      link.integrity = res.integrity
     }
 
-    function toggleOffline() {
-        rootEl.classList.toggle('offline')
+    link.rel = 'stylesheet'
+    link.href = res.href
+    head.appendChild(link)
+  }
+
+  function toggleOffline() {
+    rootEl.classList.toggle('offline')
+  }
+  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+  }
+  /*--------------------------------------------------------------------------*/
+
+  {% assign resources = site.data.init.array %}
+  {% for res in site.vendor.css %}
+    {% assign object = res | jsonify %}
+    {% assign resources = resources | push:object %}
+  {% endfor %}
+
+  // Add asynchronous style sheets.
+  [{{ resources | join:',' }}].forEach(addStyleSheet)
+
+  {% if jekyll.environment == 'production' %}
+  // Register service worker.
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+  }
+  // Fallback to AppCache.
+  else if ('applicationCache' in root) {
+    var iframe = document.createElement('iframe')
+    iframe.style.display = 'none'
+    iframe.src = '/appcache.html'
+    document.body.appendChild(iframe)
+  }
+  // Initialize Google Analytics.
+  if (navigator.onLine) {
+    var accounts = {{ site.google_analytics.accounts | jsonify }}
+    var commands = {{ site.google_analytics.commands | jsonify }}
+
+    commands[0][1] = accounts[location.hostname]
+
+    root[root.GoogleAnalyticsObject = '_ga'] = {
+      'l': Date.now(),
+      'q': commands
     }
+    var script = document.createElement('script')
+    script.src = '{{ site.google_analytics.href }}'
+    head.appendChild(script)
+  }
+  {% endif %}
 
-    /*--------------------------------------------------------------------------*/
-
-    // Add asynchronous style sheets.
-    ;[
-        {
-            href: 'https://cdn.jsdelivr.net/fontawesome/4.7.0/css/font-awesome.min.css',
-            integrity: 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN'
-        }
-    ].forEach(addStyleSheet)
-
-    // Toggle offline status.
-    addEventListener('offline', toggleOffline)
-    addEventListener('online', toggleOffline)
-})(this)
+  // Toggle offline status.
+  addEventListener('offline', toggleOffline)
+  addEventListener('online', toggleOffline)
+}(this))
